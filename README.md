9824b37f8c29fc95e2d92ed89500d52ad13e2cb9
Tatchagon Koonkoei <o_k_t@hotmail.com>
2014-10-30 17:22:33 +0700

    Add readme and delete the code is excess

---
+++ README.md
@@ -0,0 +1,20 @@
+Pick Super Engineer
+===================
+
+Feature A: Fill point after the name from random.
+
+Feature B: Fill three point the last name from random.
+
+Feature C: Search one name from all engineers.
+
+Usage: ruby picker.rb name_for_search
+
+Result:
+
+    Display engineer name from random
+    
+    Display engineer name that fill point after the last name.
+    
+    Display engineer name that fill three point the last name.
+    
+    Display an engineer name from search.
--- picker.rb
+++ picker.rb
@@ -34,35 +34,6 @@ def featureB
   subN[0] + ' ' + subN[1][0] + '...' # return name with first next letter with '...'
 end
 
-@ranName = pick_engineer
-
-def featureA()
-  chose = Array.new()
-  addot = Array.new()
-  # push name from text file to array data
-  puts chose = @ranName
-  # random name from array data
-  addot = chose.split(' ')
-
-  formatter1 = "%s.%s"
-  formatter2 = "%s.%s.%s"
-  formatter3 = "%s.%s.%s.%s"
-  if addot.length == 2
-    puts formatter1 % [addot[0], addot[1]]
-  end
-  if addot.length == 3
-    puts formatter2 % [addot[0], addot[1], addot[2]]
-  end
-  if addot.length == 4
-    puts formatter3 % [addot[0], addot[1], addot[2], addot[3]]
-  end
-end
-
-def featureB
-  subN = @ranName.split(' ') # split sub name with space bar
-  subN[0] + ' ' + subN[1][0] + '...' # return name with first next letter with '...'
-end
-
 def featureC(name)
   if ARGV.empty?
     exit
